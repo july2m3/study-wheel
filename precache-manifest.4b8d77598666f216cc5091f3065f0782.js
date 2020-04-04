ae0c1ee1097abb16439b240a7996fbaa63839470
Julio <july2m3@gmail.com>
2020-04-03 21:46:44 -0500

    Updates

---
+++ precache-manifest.4b8d77598666f216cc5091f3065f0782.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f77ef79c5e3890db0e8fcf6646206a",
+    "url": "/study-wheel/index.html"
+  },
+  {
+    "revision": "eac9c68e721888d7f1a8",
+    "url": "/study-wheel/static/css/main.1bc5b445.chunk.css"
+  },
+  {
+    "revision": "c92fa6429471a2e9ec0b",
+    "url": "/study-wheel/static/js/2.0aee9b5c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/study-wheel/static/js/2.0aee9b5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac9c68e721888d7f1a8",
+    "url": "/study-wheel/static/js/main.f4334c12.chunk.js"
+  },
+  {
+    "revision": "bf48a63e3464efbc4824",
+    "url": "/study-wheel/static/js/runtime-main.2a7b9098.js"
+  }
+]);
