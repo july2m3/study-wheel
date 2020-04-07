f4ac92a7d736ff147520c9092244e52e97143e37
Julio <july2m3@gmail.com>
2020-04-06 22:33:58 -0500

    Refactored some code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import Collapse from './Collapse';
 import Title from './Title';
 import Form from './Form';
-
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './styles.css';
 
@@ -23,16 +23,16 @@ class App extends React.Component<any, any> {
       options: ['HTML/CSS', 'Vanilla JS', 'React', 'Node', 'Hackerrank'],
       colors: ['#3FB8AF', '#7FC7AF', '#DAD8A7', '#FF9E9D', '#FF3D7F'],
       colorOptions: [],
-      test: '',
     };
     this.updateAppOptions = this.updateAppOptions.bind(this);
   }
 
+  //  set up space bar event handler and color pallete
   componentDidMount() {
     document.addEventListener('keydown', this.handleKeyDown);
 
-    //color pallette from here https://codepen.io/dropside/pen/KkLaH
-    let colorArray = [];
+    //  color pallette from here https://codepen.io/dropside/pen/KkLaH
+    const colorArray = [];
     let newColor = '';
 
     for (let i = 0; i < this.state.options.length; i++) {
@@ -45,12 +45,12 @@ class App extends React.Component<any, any> {
     this.setState(() => ({ colorOptions: colorArray }));
   }
 
-  //if options are updated, redraw the canvas
+  //  if options are updated, redraw the canvas
   componentDidUpdate() {
     this.draw();
   }
 
-  //add a new option on to state and wheel
+  //  add a new option on to state and wheel
   addOption = (e: any) => {
     e.preventDefault();
     this.setState(() => ({
@@ -62,64 +62,51 @@ class App extends React.Component<any, any> {
 
   handleKeyDown = (event: KeyboardEvent) => {
     switch (event.keyCode) {
-      case 32: //space key
+      case 32: // space key
         this.spinWheel();
-        console.log('spinning wheel');
         break;
       default:
         break;
     }
   };
 
-  //redraw the wheel, options, and picker
-  draw = () => {
-    console.log('redrawing');
-    let canvas = document.querySelector('canvas');
-    let ctx = canvas.getContext('2d');
+  //  redraw the wheel, options, and picker
+  draw = ({ options, colors } = this.state): void => {
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth - 100;
     canvas.height = window.innerHeight - 100;
 
     drawWheel(ctx);
     drawPicker(canvas, ctx);
-    drawOptions(
-      canvas,
-      ctx,
-      Object.values(this.state.options),
-      this.state.colors,
-    );
+    drawOptions(canvas, ctx, Object.values(options), colors);
   };
 
-  updateAppOptions = (optionName: string, optionValue: string) => {
-    let data = this.state.options;
-    data[optionName] = optionValue;
-
-    this.setState(() => ({ options: data }));
+  //  Update options with new value
+  updateAppOptions = (optionName: string, optionValue: string): void => {
+    const { options } = this.state;
+    options[optionName] = optionValue;
+    this.setState(() => ({ options }));
     this.draw();
   };
 
-  //redraws the wheel to make it appear to spin a minimum of 3+ seconds
-  spinWheel = () => {
-    let canvas = document.querySelector('canvas');
-    let ctx = canvas.getContext('2d');
+  //  redraws the wheel to make it appear to spin a minimum of 3+ seconds
+  spinWheel = ({ options, colors } = this.state): void => {
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
     if (this) {
-      spinWheel(
-        canvas,
-        ctx,
-        Object.values(this.state.options),
-        this.state.colors,
-      );
+      spinWheel(canvas, ctx, Object.values(options), colors);
     }
   };
 
-  //handle submission of form component to update options
-  handleFormSubmit = (e: any) => {
+  //  handle submission of form component to update options
+  handleFormSubmit = (e: any, { options } = this.state): void => {
     e.preventDefault();
     const form = document.querySelector('form');
     const formData = new FormData(form);
+    const updatedOptions = [];
 
-    let updatedOptions = [];
-
-    for (let i = 0; i < this.state.options.length; i++) {
+    for (let i = 0; i < options.length; i++) {
       updatedOptions.push(formData.get(i.toString()));
     }
 
@@ -127,7 +114,7 @@ class App extends React.Component<any, any> {
     this.draw();
   };
 
-  render() {
+  render({ options } = this.state) {
     return (
       <>
         <Title title="Study Wheel" />
@@ -137,7 +124,7 @@ class App extends React.Component<any, any> {
           </Collapse>
           <Form
             updateApp={this.updateAppOptions}
-            optionsArray={this.state.options}
+            optionsArray={options}
             addOption={this.addOption}
             handleSubmit={this.handleFormSubmit}
           />
